c9b66e6e0645b26e939418e0539938ec9238fa3c
Olga AI Workforce <tech@olga-ai.com>
2026-04-06 19:06:52 -0300

    Refactor routing in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,9 @@ import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import HomeLivonius from "./pages/HomeLivonius";
 import HomeLivo from "./pages/HomeLivo";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
-import ProdutoRCO from "./pages/ProdutoRCO";
 import ProdutoSolar from "./pages/ProdutoSolar";
 import ProdutoGarantia from "./pages/ProdutoGarantia";
 import ProdutoAgro from "./pages/ProdutoAgro";
@@ -18,27 +16,25 @@ import Sinistro from "./pages/Sinistro";
 import CotacaoConversacional from "./pages/CotacaoConversacional";
 import CadastroCorretor from "./pages/CadastroCorretor";
 import SinistroConversacional from "./pages/SinistroConversacional";
+
 function Router() {
-  // make sure to consider if you need authentication for certain routes
   return (
     <Switch>
-      <Route path={"/"} component={HomeLivonius} />
+      <Route path={"/"} component={HomeLivo} />
+      <Route path={"/livo"} component={HomeLivo} />
       <Route path={"/sinistro"} component={Sinistro} />
       <Route path="/cotacao" component={CotacaoConversacional} />
       <Route path="/cotar" component={CotacaoConversacional} />
-      <Route path={"/livonius"} component={HomeLivonius} />
-      <Route path="/livo" component={HomeLivo} />
       <Route path="/blog" component={Blog} />
       <Route path="/blog/:slug" component={BlogPost} />
-      
-      {/* Product Routes */}
-      <Route path="/livonius/rco" component={ProdutoRCO} />
+
+      {/* Product Routes — Livo */}
       <Route path="/livo/solar" component={ProdutoSolar} />
       <Route path="/livo/garantia" component={ProdutoGarantia} />
       <Route path="/livo/agro" component={ProdutoAgro} />
       <Route path="/livo/engenharia" component={ProdutoEngenharia} />
       <Route path="/livo/saude" component={ProdutoSaude} />
-      
+
       {/* Cadastro e Sinistro */}
       <Route path="/corretor" component={CadastroCorretor} />
       <Route path="/parceiro" component={CadastroCorretor} />
